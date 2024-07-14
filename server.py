6360b941d095b156677afc465a383cade5d9d3d5
Alexander Godov <godovav@oiate.ru>
2024-07-14 10:54:05 +0300

    Add POST function body

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import flask
 from flask.views import MethodView
 
-from models import Advertisement, User, Session
+from models import Advertisement, Session
 
 app = flask.Flask('advertisement_service')
 
@@ -10,16 +10,18 @@ def before_request() -> None:
     flask.request.session = Session()
 
 @app.after_request
-def after_request():
+def after_request(response: flask.Response) -> None:
     flask.request.session.close()
+
+    return response
 
 def create_advertisement(advertisement):
     flask.request.session.add(advertisement)
+    flask.request.session.commit()
 
 
 class AdvertisementView(MethodView):
     def get(self):
-        advertisement = ...
         pass
 
     def post(self):
@@ -27,10 +29,16 @@ class AdvertisementView(MethodView):
         advertisement = Advertisement(**advertisement_data)
         create_advertisement(advertisement)
 
-        return flask.jsonify(advertisement.__dict__)
+        return flask.jsonify(advertisement.dict)
 
     def patch(self):
         pass
 
     def delete(self):
         pass
+
+
+advertisement_view = AdvertisementView.as_view('advertisement')
+app.add_url_rule('/', methods=['POST'], view_func=advertisement_view)
+
+app.run(port=8080)
