9d393b5fab05ded242667ab95b8b3673b749d563
Alexander Godov <godovav@oiate.ru>
2024-07-14 09:29:54 +0300

    Create a base structure

--- server.py
+++ server.py
@@ -1,3 +1,36 @@
 import flask
+from flask.views import MethodView
+
+from models import Advertisement, User, Session
+
+app = flask.Flask('advertisement_service')
+
+@app.before_request
+def before_request() -> None:
+    flask.request.session = Session()
+
+@app.after_request
+def after_request():
+    flask.request.session.close()
+
+def create_advertisement(advertisement):
+    flask.request.session.add(advertisement)
 
 
+class AdvertisementView(MethodView):
+    def get(self):
+        advertisement = ...
+        pass
+
+    def post(self):
+        advertisement_data = flask.request.json
+        advertisement = Advertisement(**advertisement_data)
+        create_advertisement(advertisement)
+
+        return flask.jsonify(advertisement.__dict__)
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
